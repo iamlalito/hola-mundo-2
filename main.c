252092e12abab14410f22962724c2e8dbc509332
iamlalito <165375271+iamlalito@users.noreply.github.com>
2024-09-23 19:24:36 -0700

    Add files via upload

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main ()
+{
+    printf("hola mundo"); //imprime un texto//
+    printf("\n holam"); //imprime un texto con una separacion//
+    printf("\n hola mundox \v hola mundoy");
+    printf("\n \n \n hola munda \a");
+    printf("\n \"hola mundo\"");
+}
